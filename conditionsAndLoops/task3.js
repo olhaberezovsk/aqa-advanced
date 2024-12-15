fc6ee0d52f3860a21bc4351348e4c72a2c63b41e
Olha <berezovskaolh@gmail.com>
2024-12-15 20:26:48 +0200

    HomeWork 5 - Added conditionsAndLoops folder with three files

---
+++ conditionsAndLoops/task3.js
@@ -0,0 +1,13 @@
+const multiplicationNumber = 7;
+
+for (let i = 1; i <= 10; i++) 
+    {
+        console.log(`${multiplicationNumber} x ${i} = ${multiplicationNumber * i}` )
+    }
+
+let j = 1;
+while (j <= 10)
+    {
+        console.log(`${multiplicationNumber} x ${j} = ${multiplicationNumber * j}` )
+        j++;
+    }
