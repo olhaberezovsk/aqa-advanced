44d7c423c022e814be256f53fbb4fb49e16150ed
Olha <berezovskaolh@gmail.com>
2024-12-26 17:35:19 +0200

    HomeWork #8 - Added arraysJS folder with 6 files.

---
+++ arraysJS/task2.js
@@ -0,0 +1,4 @@
+const originalArray = [4, 1, 18, 0, 121];
+const newArray = originalArray.map((number, index) => number * index);
+
+console.log(newArray);
---
+++ arraysJS/task5.js
@@ -0,0 +1,5 @@
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5, 6];
+const contactedArray = firstArray.concat(secondArray);
+
+console.log(contactedArray);
