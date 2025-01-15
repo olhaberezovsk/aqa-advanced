9ff3b4bb05bb687fd92429a4ddc794490a82c0db
Olha <berezovskaolh@gmail.com>
2025-01-15 19:16:25 +0200

    Test Commit

--- jsBasics/task3.js
+++ jsBasics/task3.js
@@ -1,6 +1,4 @@
 let age = 25;
 let result = age >=18;
 
-console.log(`The age is ${age}. Is the person adult? Answer - ${result}`)
-
-
+console.log(`The age is ${age}. Is the person adult? Answer - ${result}`);
